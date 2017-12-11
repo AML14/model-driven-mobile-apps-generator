ec4c4b8830b84d120a4d91ce16dfe4377bb84418
Alberto Martín López <alberto.martin95@gmail.com>
2017-12-11 12:04:46 +0100

    More M2T implementation

--- src-gen/App/scenes/PrimeraPantalla/index.js
+++ src-gen/App/scenes/PrimeraPantalla/index.js
@@ -55,6 +55,9 @@ class PrimeraPantalla extends Component {
                 alignItems: 'center',
               }}
             >
+              <Text style={{ fontSize: 11, color: red }}>
+                Esto es texto de ejemplo, lo que aparece arriba del todo de la pantalla
+              </Text>
             </View>
             <View style={{
                 flex: '2.0',
@@ -63,6 +66,16 @@ class PrimeraPantalla extends Component {
                 alignItems: 'center',
               }}
             >
+              <Card>
+                <CardItem header>
+                  <Text>Carta de ejemplo</Text>
+                </CardItem>
+                <CardItem>
+                  <Body>
+                    <Text>Otro componente de ejemplo</Text>
+                  </Body>
+                </CardItem>
+              </Card>
             </View>
             <View style={{ flex: '2.0', flexDirection: 'row' }}>
             <View style={{
@@ -71,6 +84,9 @@ class PrimeraPantalla extends Component {
                 alignItems: 'center',
               }}
             >
+              <Item underlined>
+                <Input placeholder="Enter text" />
+              </Item>
             </View>
             <View style={{
                 flex: '1.0',
@@ -78,6 +94,9 @@ class PrimeraPantalla extends Component {
                 alignItems: 'center',
               }}
             >
+              <Button primary style={{ alignSelf: 'center' }}>
+                <Text>Enviar</Text>
+              </Button>
             </View>
             </View>
             <View style={{
@@ -87,6 +106,64 @@ class PrimeraPantalla extends Component {
                 alignItems: 'center',
               }}
             >
+              <ScrollView>
+                <List style={{ flex: 1 }}>
+                  <ListItem style={{ marginLeft: 0, paddingLeft: 15 }}>
+                    <Left style={{ flex: 1 }}>
+                      <Icon name="md-document" style={{ color: 'blue' }} />
+                    </Left>
+                    <Body style={{ flex: 8 }}>
+                      <Text numberOfLines={1}>Elemento numero 1</Text>
+                    </Body>
+                    <Right style={{ flex: 1 }}>
+                      <Icon name="arrow-forward" style={{ color: 'black' }} />
+                    </Right>
+                  </ListItem>
+                  <ListItem style={{ marginLeft: 0, paddingLeft: 15 }}>
+                    <Left style={{ flex: 1 }}>
+                      <Icon name="md-folder" style={{ color: 'red' }} />
+                    </Left>
+                    <Body style={{ flex: 8 }}>
+                      <Text numberOfLines={1}>Elemento numero 2</Text>
+                    </Body>
+                    <Right style={{ flex: 1 }}>
+                      <Icon name="arrow-forward" style={{ color: 'black' }} />
+                    </Right>
+                  </ListItem>
+                  <ListItem style={{ marginLeft: 0, paddingLeft: 15 }}>
+                    <Left style={{ flex: 1 }}>
+                      <Icon name="md-link" style={{ color: 'green' }} />
+                    </Left>
+                    <Body style={{ flex: 8 }}>
+                      <Text numberOfLines={1}>Elemento numero 3</Text>
+                    </Body>
+                    <Right style={{ flex: 1 }}>
+                      <Icon name="arrow-forward" style={{ color: 'black' }} />
+                    </Right>
+                  </ListItem>
+                  <ListItem style={{ marginLeft: 0, paddingLeft: 15 }}>
+                    <Body style={{ flex: 9 }}>
+                      <Text numberOfLines={1}>Elemento numero 4</Text>
+                    </Body>
+                    <Right style={{ flex: 1 }}>
+                      <Icon name="arrow-forward" style={{ color: 'black' }} />
+                    </Right>
+                  </ListItem>
+                  <ListItem style={{ marginLeft: 0, paddingLeft: 15 }}>
+                    <Left style={{ flex: 1 }}>
+                      <Icon name="md-folder" style={{ color: 'red' }} />
+                    </Left>
+                    <Body style={{ flex: 9 }}>
+                      <Text numberOfLines={1}>Elemento numero 5</Text>
+                    </Body>
+                  </ListItem>
+                  <ListItem style={{ marginLeft: 0, paddingLeft: 15 }}>
+                    <Body style={{ flex: 9 }}>
+                      <Text numberOfLines={1}>Elemento numero 6</Text>
+                    </Body>
+                  </ListItem>
+                </List>
+              </ScrollView>
             </View>
           </View>
         </Content>
